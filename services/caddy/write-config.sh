e3d248326fb5d23553147118dfb5f3a412d75681
Michael Prasuhn <mdp@Michaels-MBP.inside.prickels.casa>
2023-10-26 23:26:38 -0400

    More cleanup for caddy

--- services/caddy/write-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e -u -o pipefail
-
-if [ -n '$CADDY_CONFIG' ]; then
-  echo "Setting up caddy config from ENV"
-  echo "$CADDY_CONFIG" > /etc/caddy/Caddyfile
-  cat /etc/caddy/Caddyfile
-fi
